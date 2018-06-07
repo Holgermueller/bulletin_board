125ef9a78d9692c40b928dd1aff3e7112da50c31
Jatin.Kapoor <jatin.kapoor@target.com>
2018-06-06 22:21:06 -0500

    teking out validations

--- routes/login.js
+++ routes/login.js
@@ -24,17 +24,9 @@ router.post('/', passport.authenticate('local', {
 }));
 
 router.post('/register', (req, res, next) => {
-  req.checkBody('username', 'Username field cannot be empty.').notEmpty();
-  req.checkBody('username', 'Username must be between 4-15 characters long.').len(4, 15);
-  req.checkBody('email', 'The email you entered is invalid, please try again.').isEmail();
-  req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4, 100);
-  req.checkBody('password', 'Password must be between 8-100 characters long.').len(8, 100);
-  req.checkBody('password', 'Password must include one lowercase character, one uppercase character, a number, and a special character.').matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
-  req.checkBody('confirmPassword', 'Password must be between 8-100 characters long.').len(8, 100);
+
   req.checkBody('confirmPassword', 'Passwords do not match, please try again.').equals(req.body.password);
-  req.checkBody('username', 'Username can only contain letters, numbers, or underscores.').matches(/^[A-Za-z0-9_-]+$/, 'i');
-
-
+  
   const errors = req.validationErrors();
   if (errors) {
     res.render('login', {
