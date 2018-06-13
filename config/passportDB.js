ec05e2e719206bff55b2d56e01864f7b62946c5e
Joan Marie Verba <verba001@umn.edu>
2018-06-12 19:28:09 -0500

    restore

--- config/passportDB.js
+++ config/passportDB.js
@@ -3,7 +3,7 @@ module.exports = {
     "host": "localhost",
     "port": 3306,
     "user": "root",
-    "password": "loremipsum",
+    "password": "hello123",
     "database": "bulletin_board"
   },
   "optionsProd": {
